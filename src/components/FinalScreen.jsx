54ebc727625c80d06050a74890e97dbda87c57f3
JMBuccolini <juanmateobuccolini@gmail.com>
2024-04-16 13:28:49 -0300

    cambios css finalscreen

--- src/components/FinalScreen.jsx
+++ src/components/FinalScreen.jsx
@@ -8,18 +8,24 @@ export default function FinalScreen({ timeRemaining }) {
 
 
     return (
-        <div>
-            <div>
+        <div className="flex flex-col justify-center items-center">
+            <div className="text-white text-[20px]">
 
                 <p>¡Felicitaciones! Terminaste el juego</p>
                 <p>Tu puntuación es: {score}</p>
             </div>
 
-        <div>
-            <button>Intentar de nuevo</button>
-            <button>Ver tabal de puntajes</button>
-            <button>ir a MELI</button>
-        </div>
+            <div>
+                <button className={`py-[14px] px-[20px] text-white bg-blue-400 hover:bg-blue-600 rounded-lg`}>
+                    Intentar de nuevo
+                </button>
+                <button className={`py-[14px] px-[20px] text-white bg-blue-400 hover:bg-blue-600 rounded-lg`}>
+                    Ver tabla de puntajes
+                </button>
+                <button className={`py-[14px] px-[20px] text-white bg-blue-400 hover:bg-blue-600 rounded-lg`}>
+                    Ir a MELI
+                </button>
+            </div>
 
         </div>
     )
