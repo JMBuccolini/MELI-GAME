d4b1581f9107ddd48e54c7aa0d6f1732453e51a7
JMBuccolini <juanmateobuccolini@gmail.com>
2024-04-22 22:33:56 -0300

    nextjs config fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
+};
 
-module.exports = {
-    experimental: {
-      missingSuspenseWithCSRBailout: false,
-    },
-  }
-
+export default nextConfig;
