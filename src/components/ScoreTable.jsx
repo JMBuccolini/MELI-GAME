6852241a13ccc80d99e5ffaad4a780f5a0266d6e
JMBuccolini <juanmateobuccolini@gmail.com>
2024-04-22 22:13:29 -0300

    suspense boundary

--- src/components/ScoreTable.jsx
+++ src/components/ScoreTable.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect, useState, Suspense } from "react";
 import { useRouter } from "next/navigation";
 import { useSearchParams } from "next/navigation";
 import Swal from "sweetalert2";
@@ -9,13 +9,10 @@ import axios from "axios";
 export default function ScoreTable() {
   const [dbData, setDbData] = useState();
   const router = useRouter();
-  const searchParams = useSearchParams();
-  const score = searchParams.get("score");
 
   const handleRetry = () => {
-    router.push('/')
-};
-
+    router.push('/');
+  };
 
   const getScoreTable = async () => {
     try {
@@ -28,6 +25,9 @@ export default function ScoreTable() {
   };
 
   useEffect(() => {
+    const searchParams = useSearchParams();
+    const score = searchParams.get("score");
+
     Swal.fire({
       title: "Coloca tu nombre para guardar el puntaje!",
       input: "text",
@@ -84,38 +84,40 @@ export default function ScoreTable() {
   }, [router]);
 
   return (
-    <div className="flex flex-col justify-center items-center mt-14">
-      <div className="flex flex-col justify-center items-center">
-        <p className="text-white text-[60px]">¡Gracias por haber jugado! </p>
-        <p className="text-white text-[50px]">Espero que te hayas divertido</p>
+    <Suspense fallback={<div>Loading...</div>}>
+      <div className="flex flex-col justify-center items-center mt-14">
+        <div className="flex flex-col justify-center items-center">
+          <p className="text-white text-[60px]">¡Gracias por haber jugado! </p>
+          <p className="text-white text-[50px]">Espero que te hayas divertido</p>
+        </div>
+        <h2 className="text-white text-[50px] font-extrabold mt-14">
+          {" "}
+          TABLA DE POSICIONES
+        </h2>
+        <button
+          onClick={() => handleRetry()}
+          className={`py-[14px] px-[20px] text-white bg-blue-400 hover:bg-blue-600 rounded-lg hover:scale-110 transition-all duration-500 ease-in-out mt-8 mb-8`}
+        >
+          Intentar de nuevo
+        </button>
+        <div className="flex justify-center bg-yellow-300 items-center w-[800px] px-14 py-14">
+          <table className="w-full">
+            <tbody>
+              {dbData &&
+                dbData.map((row, index) => (
+                  <tr
+                    key={index}
+                    className="flex justify-between items-center border-2 p-4 border-black mb-4"
+                  >
+                    <td>{index + 1}</td>
+                    <td>{row.name}</td>
+                    <td>{row.score}</td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+        </div>
       </div>
-      <h2 className="text-white text-[50px] font-extrabold mt-14">
-        {" "}
-        TABLA DE POSICIONES
-      </h2>
-      <button
-        onClick={() => handleRetry()}
-        className={`py-[14px] px-[20px] text-white bg-blue-400 hover:bg-blue-600 rounded-lg hover:scale-110 transition-all duration-500 ease-in-out mt-8 mb-8`}
-      >
-        Intentar de nuevo
-      </button>
-      <div className="flex justify-center bg-yellow-300 items-center w-[800px] px-14 py-14">
-        <table className="w-full">
-          <tbody>
-            {dbData &&
-              dbData.map((row, index) => (
-                <tr
-                  key={index}
-                  className="flex justify-between items-center border-2 p-4 border-black mb-4"
-                >
-                  <td>{index + 1}</td>
-                  <td>{row.name}</td>
-                  <td>{row.score}</td>
-                </tr>
-              ))}
-          </tbody>
-        </table>
-      </div>
-    </div>
+    </Suspense>
   );
 }
