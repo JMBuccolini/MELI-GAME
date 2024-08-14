34355f9889a8329b499a2e536918292f3c6e3555
JMBuccolini <juanmateobuccolini@gmail.com>
2024-08-14 07:57:51 -0300

    baseURL frontend axios

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: 'http://localhost:5050/api',
+    baseURL: 'https://meli-game-2.onrender.com/api',
     withCredentials: true
 })
 
