035703b8f4ddbcd6b6f68863cd3c5877c75850ce
JMBuccolini <juanmateobuccolini@gmail.com>
2024-08-14 22:45:14 -0300

    register changes

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -15,9 +15,10 @@ export default function Register() {
   const { signup, isAuthenticated, errors: registerErrors } = useAuth();
   const router = useRouter();
   const [selectImg, setSelectImg] = useState("");
-
+const [loader, setLoader] = useState(false)
   const onSubmit = handleSubmit(async (values) => {
-    signup(values);
+    setLoader(true)
+    await signup(values);
     router.push("/login");
   });
 
@@ -167,9 +168,35 @@ export default function Register() {
           </div>
           <button
             type="submit"
-            className="rounded-lg px-[20px] py-[10px] text-[18px] text-white border-2 bg-[#3483fa] border-blue-500 w-[100px] hover:bg-blue-600 hover:scale-110 transition-all duration-500 ease-in-out mb-14 mt-4 flex items-center justify-center"
+            className="rounded-lg px-[20px] py-[10px] text-[18px] text-white border-2 bg-[#3483fa] border-blue-500 w-full hover:bg-blue-600 hover:scale-110 transition-all duration-500 ease-in-out mb-14 mt-4"
           >
-            Register
+            {loader ? (
+              <p className="flex">
+                <svg
+                  className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <circle
+                    className="opacity-25"
+                    cx="12"
+                    cy="12"
+                    r="10"
+                    stroke="currentColor"
+                    strokeWidth="4"
+                  />
+                  <path
+                    className="opacity-75"
+                    fill="currentColor"
+                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                  />
+                </svg>
+                <span>...espere un momento</span>
+              </p>
+            ) : (
+              "Registrarse"
+            )}
           </button>
         </form>
 
